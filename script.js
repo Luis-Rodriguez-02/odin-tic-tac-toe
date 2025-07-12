c8c76310c3a31701a075410847e41d7406a39f30
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2025-07-12 12:50:02 -0400

    Finish checkWin function

--- script.js
+++ script.js
@@ -26,29 +26,117 @@
   Check for a winner. 
 */
 
-function gameBoard() {
+const Gameboard = function () {
   // Create a function to make a gameBoard and then populate the cells with empty strings.
 
   const rows = 3;
   const cols = 3;
   const board = [];
 
+  let spotsTaken = 0;
+
   for (let i = 0; i < rows; i++) {
-      board[i] = [];
-      for (let j = 0; j < cols; j++) {
-        board[i].push("x");
+    board[i] = [];
+    for (let j = 0; j < cols; j++) {
+      board[i].push(""); // "" denotes empty cell
     }
   }
 
+  const printBoard = () => {
+    for (let i = 0; i < rows; i++) {
+      console.log(board[i]);
+    }
+  };
   // Method for UI to get the current state to render
   const getBoard = () => board;
-
-  const 
-  
-    
+  // Method to check if spot to place marker is valid
+  const isValidSpot = (row, col) => {
+    if (row < 0 || row > 2 || col < 0 || col > 2) {
+      return false;
+    } else if (isFull() || board[row][col] !== "") {
+      return false;
+    } else {
+      return true;
+    }
+  };
+  // Method to place marker on the board
+  const placeMarker = (row, col, marker) => {
+    // We must check if the current spot is a valid spot.
+    if (isValidSpot(row, col)) {
+      board[row][col] = marker;
+      console.log(`Successful placement at ${row}, ${col}`);
+      spotsTaken++;
+      checkWin(row, col, marker);
+    } else {
+      console.log("Invalid placement");
+    }
   };
 
+  // Checks if the board is full
+  const isFull = () => {
+    return spotsTaken == 9;
+  };
 
-const game = gameBoard();
-game.rows = 0;
-console.log(game.board);
+  // checks if after current placement someone has won
+  const checkWin = (row, col, marker) => {
+    const checkRow = () => {
+      for (let colIndex = 0; colIndex < cols; colIndex++) {
+        if (board[row][colIndex] !== marker) {
+          return false;
+        }
+      }
+      return true;
+    };
+
+    const checkCol = () => {
+      for (let rowIndex = 0; rowIndex < rows; rowIndex++) {
+        if (board[rowIndex][col] !== marker) {
+          return false;
+        }
+      }
+      return true;
+    };
+
+    const checkDiagonal = () => {
+      // check for main diagonal -> top left to bottom right
+      const isOnDiagonal = () => {
+        return row + col == 2 || row == col;
+      };
+
+      if (!isOnDiagonal()) return false;
+
+      if (row == col) {
+        if (
+          board[0][0] === marker &&
+          board[1][1] === marker &&
+          board[2][2] === marker
+        ) {
+          return true;
+        }
+      }
+      // check for anti-diagonal -> top right to bottom left
+      if (row + col == 2) {
+        if (
+          board[0][2] === marker &&
+          board[1][1] === marker &&
+          board[2][0] === marker
+        ) {
+          return true;
+        }
+      }
+      // not on main or anti diagonals so no winning condition
+      return false;
+    };
+
+    return checkRow() || checkCol() || checkDiagonal();
+  };
+
+  return {
+    placeMarker,
+    printBoard,
+  };
+};
+
+const testGame = Gameboard();
+
+testGame.printBoard();
